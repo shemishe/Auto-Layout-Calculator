3544cea972fc311950881a3069251c0dafa0b036
Sherman Shi <shermanmishi@icloud.com>
2018-12-29 06:13:50 -0800

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Sherman Shi on 12/29/18.
+//  Copyright © 2018 Sherman Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
